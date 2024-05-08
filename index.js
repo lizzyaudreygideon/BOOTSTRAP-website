39766640e8502c581b009151fe16eeec11df053f
audreylizzy <lizzyaudreygideon@gmail.com>
2024-05-08 14:09:03 -0700

    boot

--- index.js
+++ index.js
@@ -7,3 +7,22 @@ document.addEventListener('scroll',() => {
         navbar.classList.remove('scrolled');
     }
 })
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    var links = document.querySelectorAll('#menu a');
+    var menu = document.getElementById('menu');
+    
+    links.forEach(function(link) {
+      link.addEventListener('click', function(event) {
+        
+        menu.style.display = 'none';
+
+        window.scrollTo(0, 0);
+       
+        setTimeout(function() {
+          menu.style.display = '';
+        }, 5000);
+      });
+    });
+  });
